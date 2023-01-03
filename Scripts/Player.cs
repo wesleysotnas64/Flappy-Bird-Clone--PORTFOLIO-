ad929e74f6bcc18f8f0701d2d8cc2f2b69f28d6b
wesleysotnas64 <wesleysotnas64@gmail.com>
2023-01-02 21:24:53 -0300

    feat: Player com pulo e direção implementada.

---
+++ Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    CharacterController controller;
+    public Sprite sprite;
+
+    public Vector2 yVelocity;
+    public float maxHeight;
+    public float jumpSpeed;
+    public float timeToPeak;
+
+    public float gravity;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        GravityCalc();
+    }
+
+    void Update()
+    {
+        if(sprite.live)
+        {
+
+            yVelocity += gravity * Time.deltaTime * Vector2.down;
+
+            if (Input.GetKeyDown("space"))
+            {
+                yVelocity = jumpSpeed * Vector2.up;
+            } 
+
+            controller.Move(yVelocity);
+        }
+    }
+
+    private void GravityCalc()
+    {
+        gravity = (2 * maxHeight) / (Mathf.Pow(timeToPeak, 2));
+        jumpSpeed = gravity * timeToPeak;
+    }
+}
---
+++ Scripts/Sprite.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite : MonoBehaviour
+{
+
+    public bool live;
+    public Vector2 lookAt;
+    public Vector2 lookUp;
+    public Vector2 lookDown;
+
+    public float velocityLerp;
+    public float timeLerp;
+
+    void Awake()
+    {
+        live = false;
+        lookUp   = new Vector2(1,  1).normalized;
+        lookDown = new Vector2(1, -1).normalized;
+    }
+
+    void Update()
+    {
+        timeLerp += Time.deltaTime;
+        if (Input.GetKeyDown("space"))
+        {
+            live = true;
+            timeLerp = 0;
+        }
+
+        if (live)
+        { 
+            float t = Mathf.Min(timeLerp * velocityLerp, 1);
+            lookAt = Vector2.Lerp(lookUp, lookDown, t);
+
+            transform.right = lookAt;
+        }
+    }
+}
